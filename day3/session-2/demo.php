956a84965f7c053106dc3a95f7444bc6adc90573
madblocksgit <72651078+madblocksgit@users.noreply.github.com>
2021-06-09 17:20:16 +0530

    Add files via upload

---
+++ day3/session-2/demo.php
@@ -0,0 +1,8 @@
+<?php
+ echo 'Hello World';
+ echo '<br/>';
+
+ echo $_POST['uname'];
+ echo '<br/>';
+ echo $_POST['msg'];
+?>
---
+++ day3/session-2/script.js
@@ -0,0 +1,8 @@
+
+function readData() {
+	// to store the data from HTML
+	var name;
+	name=document.getElementById('name').value;
+	document.getElementById('outname').innerHTML=name;
+	//alert(name);
+}
